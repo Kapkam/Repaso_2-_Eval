ab4e06b9bd3b1010cfd3660b6f0807f5d58f06e3
Kapkam <adrinro@gmail.com>
2020-02-04 21:45:15 +0100

    Login sin acabar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,20 +18,56 @@
   <body>
     <div class="container text-center">
       <!-- Navbar -->
-      <ul class="nav sticky-top bg-white">
-        <li class="nav-item">
-          <a class="nav-link active" href="{{ route('welcome') }}">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Cafes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Stuff</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1">Login</a>
-        </li>
-      </ul>
+      <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
+          <div class="container">
+              <a class="navbar-brand" href="{{ url('/') }}">
+                  Starbucks
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <!-- Left Side Of Navbar -->
+                  <ul class="navbar-nav mr-auto">
+
+                  </ul>
+
+                  <!-- Right Side Of Navbar -->
+                  <ul class="navbar-nav ml-auto">
+                      <!-- Authentication Links -->
+                      @guest
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                          </li>
+                          @if (Route::has('register'))
+                              <li class="nav-item">
+                                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                              </li>
+                          @endif
+                      @else
+                          <li class="nav-item dropdown">
+                              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  {{ Auth::user()->name }} <span class="caret"></span>
+                              </a>
+
+                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                  <a class="dropdown-item" href="{{ route('logout') }}"
+                                     onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
+
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      @csrf
+                                  </form>
+                              </div>
+                          </li>
+                      @endguest
+                  </ul>
+              </div>
+          </div>
+      </nav>
       <hr class="mb-5 mt-5">
       <!-- Carrousel -->
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
